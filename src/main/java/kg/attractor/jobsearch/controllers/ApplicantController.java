76c67f708963b6fd3ee6951361527156384ef0f6
Sedep <yagaminezukochan@gmail.com>
2024-07-23 17:43:01 +0600

    Создала контроллеры для выполнения задач

---
+++ src/main/java/kg/attractor/jobsearch/controllers/ApplicantController.java
@@ -0,0 +1,44 @@
+package kg.attractor.jobsearch.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("Applicants")
+public class ApplicantController {
+
+    @PostMapping("/createResume")
+    public String createResume() {
+        return "Creating a Resume";
+    }
+
+    @PutMapping("/editResume/{id}")
+    public String editResume(@PathVariable Long id) {
+        return "Editing Resume with ID: ";
+    }
+
+    @DeleteMapping("/deleteResume/{id}")
+    public String deleteResume(@PathVariable Long id) {
+        return "Deleting Resume with ID: " + id;
+    }
+
+    @GetMapping("/searchActiveVacancies")
+    public String searchActiveVacancies() {
+        return "Searching for all active Vacancies";
+    }
+
+    @GetMapping("/searchVacancyByCategory")
+    public String searchVacancyByCategory(@RequestParam String category) {
+        return "Searching Vacancies in Category: " + category;
+    }
+
+    @PostMapping("/applyForVacancy/{vacancyId}")
+    public String applyForVacancy(@PathVariable Long vacancyId) {
+        return "Applying for Vacancy with ID: " + vacancyId;
+    }
+
+    @GetMapping("/searchEmployer")
+    public String searchEmployer(@RequestParam String employerName) {
+        return "Searching Employer with name: " + employerName;
+    }
+
+}
---
+++ src/main/java/kg/attractor/jobsearch/controllers/EmployerController.java
@@ -0,0 +1,44 @@
+package kg.attractor.jobsearch.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("employeers")
+public class EmployerController {
+
+    @PostMapping("/createVacancy")
+    public String createVacancy() {
+        return "Creating a Vacancy";
+    }
+
+    @PutMapping("/editVacancy")
+    public String editVacancy() {
+        return "Editing a Vacancy";
+    }
+
+    @DeleteMapping("/deleteVacancy/{id}")
+    public String deleteVacancy(@PathVariable Long id) {
+        return "Deleting a Vacancy" + id;
+    }
+
+    @GetMapping("/searchResume")
+    public String searchResume() {
+        return "Searching for Resumes";
+    }
+
+    @GetMapping("/searchByCategory/{category}")
+    public String searchByCategory(@RequestParam String category) {
+        return "Searching Resumes by Category" + category;
+    }
+
+    @GetMapping("/searchResponse/{vacancyId}")
+    public String searchResponse(@PathVariable Long vacancyId){
+        return "Search for applicants who have responded to the vacancy" + vacancyId;
+    }
+
+
+    @GetMapping("/searchingApplicants/{employerName}")
+    public String searchApplicant(@RequestParam String employerName){
+        return "Searching Applicants" + employerName;
+    }
+}
