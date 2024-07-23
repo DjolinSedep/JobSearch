a5e19640d70f534c160aa268694bfe405fd6feb8
Sedep <yagaminezukochan@gmail.com>
2024-07-23 18:41:44 +0600

    Обновила image controller

--- src/main/java/kg/attractor/jobsearch/controllers/ImageController.java
+++ src/main/java/kg/attractor/jobsearch/controllers/ImageController.java
@@ -2,14 +2,16 @@ package kg.attractor.jobsearch.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("images")
 @RestController
 public class ImageController {
 
      @GetMapping("upload")
-    public String uploadImage(){
+    public String uploadImage(@RequestParam("file") MultipartFile file){
         return "Uploading a user's avatar";
     }
 }
